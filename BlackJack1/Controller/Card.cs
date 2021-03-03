b3a17909d55f6997d6ab60a5851237cce60cfe57
mathilda serneblad <mathilda.serneblad@icloud.com>
2021-03-03 14:04:04 +0100

    changes

--- BlackJack1/Controller/Card.cs
+++ BlackJack1/Controller/Card.cs
@@ -4,40 +4,9 @@ using System.Text;
 
 namespace BlackJack1.Controller
 {
-    class Card
+    public class Card
     {
-        public enum Suit
-        {
-            Clubs,
-            Spades,
-            Diamonds,
-            Hearts
-        }
-        public enum Face
-        {
-            Ace,
-            Two,
-            Three,
-            Four,
-            Five,
-            Six,
-            Seven,
-            Eight,
-            Nine,
-            Ten,
-            Jack,
-            Queen,
-            King
-        }
-
-        public class Card_
-        {
-            public Suit Suit { get; }
-            public Face Face { get; }
-            public int Value { get; set; }
-        }
-
-
+        
     }
 }
 
