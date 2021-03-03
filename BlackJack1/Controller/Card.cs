5597b256d3df8bddf4d24438aa76ae99721c34a0
preeti dogra <preetidogra352@gmail.com>
2021-03-03 13:37:07 +0100

    added CARD class

---
+++ BlackJack1/Controller/Card.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackJack1.Controller
+{
+    class Card
+    {
+        public enum Suit
+        {
+            Clubs,
+            Spades,
+            Diamonds,
+            Hearts
+        }
+        public enum Face
+        {
+            Ace,
+            Two,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            Ten,
+            Jack,
+            Queen,
+            King
+        }
+
+        public class Card_
+        {
+            public Suit Suit { get; }
+            public Face Face { get; }
+            public int Value { get; set; }
+        }
+
+
+    }
+}
+
+
+
